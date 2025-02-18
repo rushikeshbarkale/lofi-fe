5da92b88c0cc7ce3bde3876b2be43853bf59197c
Rushikesh Barkale <rushikesh@turnkey.work>
2025-02-18 11:00:51 +0530

    updates to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,10 @@
 export const CLOUDINARY_BASE_URL = import.meta.env.VITE_CLOUDINARY_BASE_URL;
 
-const isDevelopment = import.meta.env.MODE === "local";
-
 // Set API base URL dynamically
-const BASE_API_URL = isDevelopment
-    ? import.meta.env.VITE_API_URL_LOCAL
-    : import.meta.env.VITE_API_URL_PROD;
+const BASE_API_URL =
+    import.meta.env.VITE_API_URL_PROD && window.location.hostname !== "localhost"
+        ? import.meta.env.VITE_API_URL_PROD
+        : import.meta.env.VITE_API_URL_LOCAL;
 
 // Append `/api/music` dynamically
 export const API_URL = `${BASE_API_URL}api/music`;
